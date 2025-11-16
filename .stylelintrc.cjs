f2e0b8094d9daa94d22457217301b89be919cf90
Jeff Tao <jatao88@gmail.com>
2025-11-16 14:43:15 -0500

    style(people): condense profile cards

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-html"],
+  extends: ["stylelint-config-html"],
   overrides: [
     {
       files: ["**/*.astro"],
